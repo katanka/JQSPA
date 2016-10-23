ad6cb4657ef4d7f504a49720c30091f1aab712d7
Andrew School <andrewschool@Andrews-MacBook-Pro.local>
2016-10-23 15:21:28 -0700

    test

--- controller.js
+++ controller.js
@@ -1,13 +1,12 @@
 $("#header > a").click(function (event) {
 	var target = event.target.attributes[0].nodeValue;
 	// if it's not a valid route, follow the link
+	event.preventDefault();
 	if (!(target in routes)) {
 		log("Not a route, following link");
-		debugger;
 		return;
 	}
-	event.preventDefault();
-
+	
 	var htmlfile = routes[target].html;
 
 	$.get(htmlfile, function(data) {
