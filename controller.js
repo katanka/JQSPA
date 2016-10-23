c0a898f5264e974b8bd55c29b61045c2b54c748d
Andrew School <andrewschool@Andrews-MacBook-Pro.local>
2016-10-23 15:19:23 -0700

    test

--- controller.js
+++ controller.js
@@ -2,6 +2,8 @@ $("#header > a").click(function (event) {
 	var target = event.target.attributes[0].nodeValue;
 	// if it's not a valid route, follow the link
 	if (!(target in routes)) {
+		log("Not a route, following link");
+		debugger;
 		return;
 	}
 	event.preventDefault();
@@ -12,3 +14,11 @@ $("#header > a").click(function (event) {
 		$("#content").html(data);
 	});
 });
+
+var debug = true;
+
+function log(str) {
+	if (debug == true) {
+		console.log(str);
+	}
+}
